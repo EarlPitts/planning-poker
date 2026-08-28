e6a3870567c2ce48eeaeaf89f05bc4949ed4dfae
Bendeguz Seres <grafitpor@gmail.com>
2026-08-28 08:04:02 +0200

    Break tests to see them run in the pipeline

--- test/Main.hs
+++ test/Main.hs
@@ -35,7 +35,7 @@ main = hspec $ do
               , sIsRevealed = revealed
               , sHost = host
               }
-       in findPlayer (pId player) state == Just player
+       in findPlayer (pId player) state == Nothing
 
   it "doesn't find non-existent player" $ do
     property $ \player others revealed host ->
