94fbb9e4beba083a988ca89ba7810d748c05cede
Bendeguz Seres <grafitpor@gmail.com>
2026-08-30 08:22:58 +0200

    Get rid of the remnants of my experiment

--- src/Web/View.hs
+++ src/Web/View.hs
@@ -7,7 +7,6 @@ import qualified Data.Text as T
 import Data.UUID
 import Lucid
 import Lucid.Base (makeAttributes)
-import Web.Style (renderedCss)
 
 import Core
 
@@ -23,7 +22,6 @@ template title body = doctypehtml_ $ do
   head_ $ do
     title_ $ toHtml title
     link_ [rel_ "stylesheet", type_ "text/css", href_ "/assets/style.css"]
-    -- style_ [type_ "text/css"] (toHtmlRaw renderedCss)
     script_
       [ src_ "https://cdn.jsdelivr.net/npm/htmx.org@2.0.10/dist/htmx.min.js"
       , integrity_ "sha384-H5SrcfygHmAuTDZphMHqBJLc3FhssKjG7w/CeCpFReSfwBWDTKpkzPP8c+cLsK+V"
